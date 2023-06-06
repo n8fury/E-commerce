c4193ceed758b723ba85a4a0322f7abcdf5314bf
n8fury <mhjoy547@yahoo.com>
2023-06-07 02:41:35 +0600

    basic route setup

--- Server/src/server.js
+++ Server/src/server.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.get('/health', (req, res) => {
+	res.status(200).send({
+		message: 'API is working',
+	});
+});
+app.get('/', (req, res) => {
+	res.status(200).send({
+		message: 'Success',
+	});
+});
+app.get('/products', (req, res) => {
+	res.status(200).send({
+		message: 'Products',
+	});
+});
+
 app.listen(3000, (req, res) => {
 	console.log('Server is running on port 3000');
 });
