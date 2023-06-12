bf9505606eb6aa44f7a0e750e87ff6cc0c1dcefe
n8fury <mhjoy547@yahoo.com>
2023-06-12 13:32:13 +0600

    added api request limit

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,9 +3,20 @@ const morgan = require('morgan');
 const body_parser = require('body-parser');
 const isLoggedin = require('../middlewares/session');
 const createError = require('http-errors');
+const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+const rateLimiter = rateLimit({
+	windowMs: 1 * 1000 * 60, // converted millisecond to second
+	rate: 5,
+	message: 'request limit expired',
+});
+
 app.use(morgan('dev'));
+app.use(xssClean());
+app.use(rateLimiter);
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(isLoggedin);
