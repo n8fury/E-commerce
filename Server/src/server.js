524db4416850e32e3644951ff63ad473fcf883c1
n8fury <mhjoy547@yahoo.com>
2023-06-14 02:29:56 +0600

    added env variables

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const app = require('./app');
+const port = process.env.SERVER_PORT || 3001;
 
-app.listen(3000, (req, res) => {
-	console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
 });
