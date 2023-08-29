8806f73e2fe01f15abe42b8989ba76a705b043c7
n8fury <mhjoy547@yahoo.com>
2023-08-29 18:47:59 +0600

    Added: Validation for seed user data

--- Server/src/routers/seedRouter.js
+++ Server/src/routers/seedRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const seedUser = require('../controllers/seedController');
+const fileUpload = require('../middlewares/fileUpload');
 const seedRouter = express.Router();
 
 // /api/seed
 
-seedRouter.get('/user', seedUser);
+seedRouter.get('/user', fileUpload.single('image'), seedUser);
 module.exports = seedRouter;
